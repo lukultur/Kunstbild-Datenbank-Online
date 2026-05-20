d29ee65ba0a11640c18c6950107fc5ac8a61ec3c
lukultur <lm@2mcon.de>
2026-05-20 13:09:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -259,7 +259,7 @@ else:
     if ansicht == "Galerieansicht":
 
         for start in range(0, len(gefiltert), 3):
-        spalten = st.columns(3)
+            spalten = st.columns(3)
 
         for i in range(3):
             if start + i >= len(gefiltert):
